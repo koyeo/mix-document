2e1da47ece9b471534ab585a9ac608f50183009b
Koyeo <im@koyeo.io>
2020-12-16 22:27:49 +0800

    edit

---
+++ docs/version/database.md
@@ -0,0 +1,7 @@
+---
+title: 数据库版本比较
+---
+
+* [数据库版本比较](/docs/version/database.md)
+* [API 版本比较](/docs/version/api.md)
+  
--- sidebars.js
+++ sidebars.js
@@ -1,21 +1,21 @@
 module.exports = {
-  someSidebar: {
-    '上手指南': ['guide', 'start', 'issue'],
-    '项目生成': [
-      'maker/project',
-      'maker/config',
-      'maker/service',
-      'maker/api',
-      'maker/method',
-      'maker/ui',
-      'maker/ui-component',
-      'maker/sdk',
-      'maker/database'],
-    '自动运行': ['runner/config', 'runner/watch', 'runner/env'],
-    // '接口调试': ['doc1', 'doc2', 'doc3'],
-    // '版本比较': ['doc1', 'doc2', 'doc3'],
-    '构建部署': ['ci/task', 'ci/version', 'ci/env', 'ci/server'],
-    // Features: ['mdx'],
-  },
+    someSidebar: {
+        '上手指南': ['guide', 'start', 'issue'],
+        '项目生成': [
+            'maker/project',
+            'maker/config',
+            'maker/service',
+            'maker/api',
+            'maker/method',
+            'maker/ui',
+            'maker/ui-component',
+            'maker/sdk',
+            'maker/database'],
+        '自动运行': ['runner/config', 'runner/watch', 'runner/env'],
+        // '接口调试': ['doc1', 'doc2', 'doc3'],
+        '版本比较': ['version/index', 'version/database', '/version/api'],
+        '构建部署': ['ci/task', 'ci/version', 'ci/env', 'ci/server'],
+        // Features: ['mdx'],
+    },
 }
 
